36c614ed00b75914ba2908187849ed0d9d5ee4a3
wyc <wyc6668205@users.noreply.github.com>
2021-01-20 15:40:51 +0800

    stream

--- src/http2/stream.cc
+++ src/http2/stream.cc
@@ -1,80 +1,104 @@
 #include "src/http2/stream.h"
-#include "src/http2/errors.h"
 
-const http2_stream::internal_stream_frame_func http2_stream::frame_funcs[] = {
-    {HTTP2_FRAME_DATA, frame_data},
-    {HTTP2_FRAME_HEADERS, frame_header},
-    {HTTP2_FRAME_PRIORITY, frame_priority},
-    {HTTP2_FRAME_RST_STREAM, frame_rst_stream},
-    {HTTP2_FRAME_PUSH_PROMISE, frame_push_promise},
-    {HTTP2_FRAME_PING, frame_ping},
-    {HTTP2_FRAME_CONTINUATION, frame_continuation},
+/* http2 status migrational table
+                                +--------+
+                        send PP |        | recv PP
+                       ,--------|  idle  |--------.
+                      /         |        |         \
+                     v          +--------+          v
+              +----------+          |           +----------+
+              |          |          | send H /  |          |
+       ,------| reserved |          | recv H    | reserved |------.
+       |      | (local)  |          |           | (remote) |      |
+       |      +----------+          v           +----------+      |
+       |          |             +--------+             |          |
+       |          |     recv ES |        | send ES     |          |
+       |   send H |     ,-------|  open  |-------.     | recv H   |
+       |          |    /        |        |        \    |          |
+       |          v   v         +--------+         v   v          |
+       |      +----------+          |           +----------+      |
+       |      |   half   |          |           |   half   |      |
+       |      |  closed  |          | send R /  |  closed  |      |
+       |      | (remote) |          | recv R    | (local)  |      |
+       |      +----------+          |           +----------+      |
+       |           |                |                 |           |
+       |           | send ES /      |       recv ES / |           |
+       |           | send R /       v        send R / |           |
+       |           | recv R     +--------+   recv R   |           |
+       | send R /  ‘----------->|        |<-----------’  send R / |
+       | recv R                 | closed |               recv R   |
+       ‘----------------------->|        |<----------------------’
+                                +--------+
+          send:   endpoint sends this frame
+          recv:   endpoint receives this frame
+          H:  HEADERS frame (with implied CONTINUATIONs)
+          PP: PUSH_PROMISE frame (with implied CONTINUATIONs)
+          ES: END_STREAM flag
+          R:  RST_STREAM frame
+*/
+enum internal_stream_event {
+    HTTP2_STREAM_EVENT_HEADERS_R = 0,
+    HTTP2_STREAM_EVENT_HEADERS_S = 1,
+    HTTP2_STREAM_EVENT_PP_R = 2,  // PUSH_PROMISE
+    HTTP2_STREAM_EVENT_PP_S = 3,  // PUSH_PROMISE
+    HTTP2_STREAM_EVENT_ES_R = 4,  // END_STREAM
+    HTTP2_STREAM_EVENT_ES_S = 5,
+    HTTP2_STREAM_EVENT_RST_R = 6,  // RST_STREAM
+    HTTP2_STREAM_EVENT_RST_S = 7,  // RST_STREAM
 };
 
-http2_stream::http2_stream(uint32_t sid, http2_connection *conn) {
-    _sid = sid;
-    _conn = conn;
-    _flow_max = 0xffff;
+const http2_stream::status event_status_table[8][7] = {
+    {http2_stream::OPEN, http2_stream::IDLE, http2_stream::HALF_CLOSED_LOCAL, http2_stream::IDLE, http2_stream::IDLE,
+     http2_stream::IDLE, http2_stream::IDLE},
+    {http2_stream::OPEN, http2_stream::HALF_CLOSED_REMOTE, http2_stream::IDLE, http2_stream::IDLE, http2_stream::IDLE,
+     http2_stream::IDLE, http2_stream::IDLE},
+    {http2_stream::RESERVED_REMOTE, http2_stream::IDLE, http2_stream::IDLE, http2_stream::IDLE, http2_stream::IDLE,
+     http2_stream::IDLE, http2_stream::IDLE},
+    {http2_stream::RESERVED_LOCAL, http2_stream::IDLE, http2_stream::IDLE, http2_stream::IDLE, http2_stream::IDLE,
+     http2_stream::IDLE, http2_stream::IDLE},
+    {http2_stream::IDLE, http2_stream::IDLE, http2_stream::IDLE, http2_stream::HALF_CLOSED_REMOTE, http2_stream::CLOSED,
+     http2_stream::IDLE, http2_stream::IDLE},
+    {http2_stream::IDLE, http2_stream::IDLE, http2_stream::IDLE, http2_stream::HALF_CLOSED_LOCAL, http2_stream::IDLE,
+     http2_stream::CLOSED, http2_stream::IDLE},
+    {http2_stream::IDLE, http2_stream::CLOSED, http2_stream::CLOSED, http2_stream::CLOSED, http2_stream::CLOSED,
+     http2_stream::CLOSED, http2_stream::IDLE},
+    {http2_stream::IDLE, http2_stream::CLOSED, http2_stream::CLOSED, http2_stream::CLOSED, http2_stream::CLOSED,
+     http2_stream::CLOSED, http2_stream::IDLE},
+};
+
+http2_stream::status get_next_status(internal_stream_event event, http2_stream::status cur_status) {
+    return event_status_table[event][cur_status] != http2_stream::IDLE ? event_status_table[event][cur_status]
+                                                                       : cur_status;
 }
 
-http2_errors http2_stream::frame_arrived(http2_frame *frame) {
-    if (frame == nullptr || frame->head.stream_id != _sid) {
-        return HTTP2_NO_ERROR;
-    }
-
-    for (size_t i = 0; i < sizeof(frame_funcs) / sizeof(internal_stream_frame_func); i++) {
-        if (frame_funcs[i].type == frame->head.type) {
-            return frame_funcs[i].func(frame);
-        }
-    }
-    return HTTP2_PROTOCOL_ERROR;
+void http2_stream::send_pp() {
+    _cur_status = get_next_status(HTTP2_STREAM_EVENT_PP_S, _cur_status);
 }
 
-http2_errors http2_stream::frame_data(http2_frame *frame) {
-    if (is_control_stream()) {
-        return HTTP2_PROTOCOL_ERROR;
-    }
-    if (frame->head.flag & HTTP2_FLAG_END_STREAM) {
-        _stream_status.handle_status(HTTP2_STREAM_EVENT_ES_R);
-    }
-    return HTTP2_NO_ERROR;
+void http2_stream::recv_pp() {
+    _cur_status = get_next_status(HTTP2_STREAM_EVENT_PP_R, _cur_status);
 }
 
-http2_errors http2_stream::frame_header(http2_frame *frame) {
-    if (frame->head.flag & HTTP2_FLAG_END_HEADERS) {
-    }
-    if (frame->head.flag & HTTP2_FLAG_END_STREAM) {
-        _stream_status.handle_status(HTTP2_STREAM_EVENT_ES_R);
-    }
-
-    if (_stream_status.handle_status(HTTP2_STREAM_EVENT_HEADERS_R)) {
-    }
-    return HTTP2_NO_ERROR;
+void http2_stream::send_h() {
+    _cur_status = get_next_status(HTTP2_STREAM_EVENT_HEADERS_S, _cur_status);
 }
 
-http2_errors http2_stream::frame_priority(http2_frame *frame) {
-    return HTTP2_NO_ERROR;
+void http2_stream::recv_h() {
+    _cur_status = get_next_status(HTTP2_STREAM_EVENT_HEADERS_R, _cur_status);
 }
 
-http2_errors http2_stream::frame_rst_stream(http2_frame *frame) {
-    if (is_control_stream()) {
-        return HTTP2_PROTOCOL_ERROR;
-    }
-    if (!_stream_status.handle_status(HTTP2_STREAM_EVENT_RST_R)) {
-        return HTTP2_PROTOCOL_ERROR;
-    }
-    return HTTP2_NO_ERROR;
+void http2_stream::send_r() {
+    _cur_status = get_next_status(HTTP2_STREAM_EVENT_RST_S, _cur_status);
 }
 
-http2_errors http2_stream::frame_push_promise(http2_frame *frame) {
-    _stream_status.handle_status(HTTP2_STREAM_EVENT_PP_R);
-    return HTTP2_NO_ERROR;
+void http2_stream::recv_r() {
+    _cur_status = get_next_status(HTTP2_STREAM_EVENT_RST_R, _cur_status);
 }
 
-http2_errors http2_stream::frame_ping(http2_frame *frame) {
-    return HTTP2_NO_ERROR;
+void http2_stream::send_es() {
+    _cur_status = get_next_status(HTTP2_STREAM_EVENT_ES_S, _cur_status);
 }
 
-http2_errors http2_stream::frame_continuation(http2_frame *frame) {
-    return HTTP2_NO_ERROR;
+void http2_stream::recv_es() {
+    _cur_status = get_next_status(HTTP2_STREAM_EVENT_ES_R, _cur_status);
 }
--- src/http2/stream.h
+++ src/http2/stream.h
@@ -1,56 +1,28 @@
 #pragma once
-#include <stdint.h>
-#include <functional>
-#include "src/http2/frame.h"
-#include "src/http2/errors.h"
-#include "src/http2/stream_status.h"
-/*
-class http2_stream_status_listener {
-public:
-    virtual void http2_stream_status_changed(http2_stream_status old_status, http2_stream_status new_status) const = 0;
-};
-*/
-class http2_connection;
+
 class http2_stream {
 public:
-    http2_stream(uint32_t sid, http2_connection *conn);
-    ~http2_stream();
-
-    bool is_control_stream() const {
-        return _sid == 0;
-    }
-    uint32_t get_sid() const {
-        return _sid;
-    }
-    void set_flow(uint32_t flow) {
-        _flow_max = flow;
-    }
-    /*
-    void set_stauts_listener(http2_stream_status_listener *listener) {
-        _listener = listener;
-    }*/
-    http2_errors frame_arrived(http2_frame *frame);
-    void close();
-
-private:
-    struct internal_stream_frame_func {
-        http2_frame_type type;
-        std::function<http2_errors(http2_frame *frame)> func;
+    enum status {
+        IDLE,
+        RESERVED_LOCAL,
+        RESERVED_REMOTE,
+        OPEN,
+        HALF_CLOSED_LOCAL,
+        HALF_CLOSED_REMOTE,
+        CLOSED,
     };
-    static const internal_stream_frame_func frame_funcs[];
 
-    http2_errors frame_data(http2_frame *frame);
-    http2_errors frame_header(http2_frame *frame);
-    http2_errors frame_priority(http2_frame *frame);
-    http2_errors frame_rst_stream(http2_frame *frame);
-    http2_errors frame_push_promise(http2_frame *frame);
-    http2_errors frame_ping(http2_frame *frame);
-    http2_errors frame_continuation(http2_frame *frame);
+    http2_stream() = default;
+
+    void send_pp();
+    void recv_pp();
+    void send_h();
+    void recv_h();
+    void send_r();
+    void recv_r();
+    void send_es();
+    void recv_es();
 
 private:
-    uint32_t _sid;
-    uint32_t _flow_max;
-    http2_connection *_conn;
-    http2_stream_status_fsm _stream_status;
-    // http2_stream_status_listener *_listener = nullptr;
+    status _cur_status = IDLE;
 };
