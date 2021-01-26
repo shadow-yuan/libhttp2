a9a23403e781205e912bf6289bcc995c5d59d12d
shadow-yuan <1092421495@qq.com>
2021-01-26 12:02:42 +0800

    flow control

--- src/http2/flow_control.cc
+++ src/http2/flow_control.cc
@@ -1,5 +1,178 @@
 #include "src/http2/flow_control.h"
+#include <limits.h>
 
-flow_control::flow_control(/* args */) {}
+static constexpr uint32_t kDefaultWindow = 65535;
+static constexpr uint32_t kFrameSize = 1048576;
+static constexpr const uint32_t kMaxWindowUpdateSize = 0x7fffffff;
 
-flow_control::~flow_control() {}
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define CLAMP(a, min, max) ((a) < (min) ? (min) : (a) > (max) ? (max) : (a))
+
+ConnectionFlowControl::ConnectionFlowControl(uint32_t sent_init_window) {
+    sent_init_window_ = sent_init_window;
+
+    remote_window_ = kDefaultWindow;
+    target_initial_window_size_ = kDefaultWindow;
+    announced_window_ = kDefaultWindow;
+
+    announced_stream_total_over_incoming_window_ = 0;
+    max_frame_size_ = kFrameSize;
+}
+
+ConnectionFlowControl::~ConnectionFlowControl() {}
+
+void ConnectionFlowControl::InitializeWindowSize() {
+    const double target = 4194304;  // pow(2, 22);
+
+    // Though initial window 'could' drop to 0, we keep the floor at 128
+    target_initial_window_size_ = static_cast<int32_t> CLAMP(target, 128, INT32_MAX);
+
+    // get bandwidth estimate and update max_frame accordingly.
+    double bw_dbl = 0;
+
+    // we target the max of BDP or bandwidth in microseconds.
+    max_frame_size_ = static_cast<int32_t> CLAMP(
+        MAX((int32_t)CLAMP(bw_dbl, 0, INT_MAX) / 1000, target_initial_window_size_), 16384, 16777215);
+}
+
+int64_t ConnectionFlowControl::TargetWindow() {
+    return static_cast<uint32_t> MIN((int64_t)((1u << 31) - 1),
+                                     announced_stream_total_over_incoming_window_ + target_initial_window_size_);
+}
+
+uint32_t ConnectionFlowControl::MaybeSendUpdate(bool writing_anyway) {
+    const uint32_t target_announced_window = static_cast<uint32_t>(TargetWindow());
+    if ((writing_anyway || announced_window_ <= target_announced_window / 2) &&
+        announced_window_ != target_announced_window) {
+        const uint32_t announce =
+            static_cast<uint32_t> CLAMP(target_announced_window - announced_window_, 0, kMaxWindowUpdateSize);
+        announced_window_ += announce;
+        return announce;
+    }
+    return 0;
+}
+
+uint32_t ConnectionFlowControl::FlushWindowUpdates() {
+    // CREATE WINDOW_UPDATE FRAME
+    return MaybeSendUpdate(true);
+}
+
+void ConnectionFlowControl::StreamSentData(int64_t size) {
+    remote_window_ -= size;
+}
+
+void ConnectionFlowControl::PreUpdateAnnouncedWindowOverIncomingWindow(int64_t delta) {
+    if (delta > 0) {
+        announced_stream_total_over_incoming_window_ -= delta;
+    }
+}
+
+void ConnectionFlowControl::PostUpdateAnnouncedWindowOverIncomingWindow(int64_t delta) {
+    if (delta > 0) {
+        announced_stream_total_over_incoming_window_ += delta;
+    }
+}
+
+bool ConnectionFlowControl::ValidateRecvData(int64_t incoming_frame_size) {
+    if (incoming_frame_size > announced_window_) {
+        return false;
+    }
+    return true;
+}
+
+void ConnectionFlowControl::CommitRecvData(int64_t incoming_frame_size) {
+    announced_window_ -= incoming_frame_size;
+}
+
+bool ConnectionFlowControl::RecvData(int64_t incoming_frame_size) {
+    if (!ValidateRecvData(incoming_frame_size)) return false;
+    CommitRecvData(incoming_frame_size);
+    return true;
+}
+
+void ConnectionFlowControl::RecvUpdate(uint32_t size) {
+    remote_window_ += size;
+}
+
+int32_t ConnectionFlowControl::MaxFrameSize() const {
+    return max_frame_size_;
+}
+
+uint32_t ConnectionFlowControl::SentInitWindow() const {
+    return sent_init_window_;
+}
+// ---------------------------------------------------
+
+StreamFlowControl::StreamFlowControl(uint32_t stream_id, ConnectionFlowControl *t)
+    : stream_id_(stream_id)
+    , tfc_(t) {
+
+    remote_window_delta_ = 0;
+    local_window_delta_ = 0;
+    announced_window_delta_ = 0;
+}
+
+StreamFlowControl::~StreamFlowControl() {
+    tfc_->PreUpdateAnnouncedWindowOverIncomingWindow(announced_window_delta_);
+}
+
+void StreamFlowControl::IncomingByteStreamUpdate(size_t max_size_hint, size_t have_already) {
+    uint32_t sent_init_window = tfc_->SentInitWindow();  // HTTP2_SETTINGS_INITIAL_WINDOW_SIZE
+    uint32_t max_recv_bytes;
+
+    if (max_size_hint >= kMaxWindowUpdateSize - sent_init_window) {
+        max_recv_bytes = kMaxWindowUpdateSize - sent_init_window;
+    } else {
+        max_recv_bytes = static_cast<uint32_t>(max_size_hint);
+    }
+
+    if (max_recv_bytes >= have_already) {
+        max_recv_bytes -= static_cast<uint32_t>(have_already);
+    } else {
+        max_recv_bytes = 0;
+    }
+
+    if (local_window_delta_ < max_recv_bytes) {
+        uint32_t add_max_recv_bytes = static_cast<uint32_t>(max_recv_bytes - local_window_delta_);
+        local_window_delta_ += add_max_recv_bytes;
+    }
+}
+
+void StreamFlowControl::SentData(int64_t outgoing_frame_size) {
+    tfc_->StreamSentData(outgoing_frame_size);
+    remote_window_delta_ -= outgoing_frame_size;
+}
+
+uint32_t StreamFlowControl::MaybeSendUpdate() {
+    if (local_window_delta_ > announced_window_delta_) {
+        uint32_t announce =
+            static_cast<uint32_t> CLAMP(local_window_delta_ - announced_window_delta_, 0, kMaxWindowUpdateSize);
+        UpdateAnnouncedWindowDelta(tfc_, announce);
+        return announce;
+    }
+    return 0;
+}
+
+void StreamFlowControl::UpdateAnnouncedWindowDelta(ConnectionFlowControl *tfc, int64_t change) {
+    tfc->PreUpdateAnnouncedWindowOverIncomingWindow(announced_window_delta_);
+    announced_window_delta_ += change;
+    tfc->PostUpdateAnnouncedWindowOverIncomingWindow(announced_window_delta_);
+}
+
+uint32_t StreamFlowControl::FlushWindowUpdates() {
+    // CREATE WINDOW_UPDATE FRAME
+    return MaybeSendUpdate();
+}
+
+bool StreamFlowControl::RecvData(int64_t incoming_frame_size) {
+    if (!tfc_->ValidateRecvData(incoming_frame_size)) return false;
+    UpdateAnnouncedWindowDelta(tfc_, -incoming_frame_size);
+    local_window_delta_ -= incoming_frame_size;
+    tfc_->CommitRecvData(incoming_frame_size);
+    return true;
+}
+
+void StreamFlowControl::RecvUpdate(uint32_t size) {
+    remote_window_delta_ += size;
+}
--- src/http2/flow_control.h
+++ src/http2/flow_control.h
@@ -2,13 +2,81 @@
 #include <stddef.h>
 #include <stdint.h>
 
-class flow_control {
+class ConnectionFlowControl final {
 public:
-    flow_control(/* args */);
-    ~flow_control();
+    explicit ConnectionFlowControl(uint32_t sent_init_window);
+    ~ConnectionFlowControl();
+
+    // Connection flow control initialization window size
+    void InitializeWindowSize();
+
+    // Returns the size of the WINDOW_UPDATE frame to be created
+    uint32_t FlushWindowUpdates();
+
+    // received a DATA frame
+    bool RecvData(int64_t incoming_frame_size);
+
+    // we have received a WINDOW_UPDATE frame for a connection
+    void RecvUpdate(uint32_t size);
+
+    int32_t MaxFrameSize() const;
 
 private:
-    int64_t _local_window_size;
-    int64_t _remote_window_size;
-    int64_t _initial_window_size;
+    uint32_t MaybeSendUpdate(bool writing_anyway);
+    int64_t TargetWindow();
+
+    // stream has been sent data
+    void StreamSentData(int64_t size);
+
+    void PreUpdateAnnouncedWindowOverIncomingWindow(int64_t delta);
+    void PostUpdateAnnouncedWindowOverIncomingWindow(int64_t delta);
+    bool ValidateRecvData(int64_t incoming_frame_size);
+    void CommitRecvData(int64_t incoming_frame_size);
+
+    uint32_t SentInitWindow() const;
+
+private:
+    uint32_t sent_init_window_;
+    int64_t remote_window_;
+    int64_t target_initial_window_size_;
+    int64_t announced_window_;
+
+    // send request +5, recv response -5
+    int64_t announced_stream_total_over_incoming_window_;
+    int32_t max_frame_size_;
+
+    friend class StreamFlowControl;
+};
+
+class StreamFlowControl final {
+public:
+    StreamFlowControl(uint32_t stream_id, ConnectionFlowControl *t);
+    ~StreamFlowControl();
+
+    // GRPC_MESSAGE header 5 bytes
+    void IncomingByteStreamUpdate(size_t max_size_hint, size_t have_already = 0);
+
+    // send DATA frame (include grpc_message header 5 bytes)
+    void SentData(int64_t outgoing_frame_size);
+
+    // Returns the size of the WINDOW_UPDATE frame to be created
+    uint32_t FlushWindowUpdates();
+
+    // we have received data from the wire
+    bool RecvData(int64_t incoming_frame_size);
+
+    // we have received a WINDOW_UPDATE frame for a stream
+    void RecvUpdate(uint32_t size);
+
+private:
+    uint32_t MaybeSendUpdate();
+
+    void UpdateAnnouncedWindowDelta(ConnectionFlowControl *tfc, int64_t change);
+
+    uint32_t stream_id_;
+    ConnectionFlowControl *tfc_;
+
+    int64_t remote_window_delta_;
+    int64_t local_window_delta_;
+    int64_t announced_window_delta_;
 };
